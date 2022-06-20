017ba44ae4302241874ecea4f61b0ac9861a79d9
sunniboi <attila.szaniszlo@gmail.com>
2022-06-20 18:18:19 +0200

    updating the state

--- mosh_react/src/components/counter.jsx
+++ mosh_react/src/components/counter.jsx
@@ -2,38 +2,57 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     //imageURL: 'https://picsum.photos/200'
   }; //any data this comp needs
 
-  constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
-  // //not used
-  // styles = {
-  //   fontSize: 50,
-  //   fontWeight: "bold",
-  // };
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  //   this.handleMinus = this.handleMinus.bind(this);
+  // }
+  // // //not used
+  // // styles = {
+  // //   fontSize: 50,
+  // //   fontWeight: "bold",
+  // // };
 
   //bind this  the event object
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
-    
+  handleMinus = (product) => {
+    console.log(product);
+    this.setState({ value: this.state.value - 1 });
+  };
+
 
   render() {
-    console.log("props",this.props);
+    console.log("props", this.props);
     return (
       <div>
+        {this.props.children}
+        {/* what is this props */}
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button
-          onClick={()=> this.handleIncrement()}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm "
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.handleMinus()}
+          className="btn btn-secondary btn-sm "
+        >
+          Decrease
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id) }
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -41,13 +60,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- mosh_react/src/components/counters.jsx
+++ mosh_react/src/components/counters.jsx
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Counter from "./counter";
 
-class Counters  extends Component {
-  state = { 
-    counters : [
-      {id:1, value:4},
-      {id:2, value:0},
-      {id:3, value:0},
-      {id:4, value:0}
-    ]
-   } ;
-  render() { 
-    return (<div>
-      {this.state.counters.map(counter => 
-      <Counter key={counter.id}  value={counter.value} selected={true} />)}
-    </div>);
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    // console.log("event handler called", counterId);
+    const counters = this.state.counters.filter(c=> c.id !== counterId);
+    this.setState({counters});
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            id={counter.id}
+            value={counter.value}
+          >
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
+        ))}
+      </div>
+    );
   }
 }
- 
-export default Counters ;
+
+export default Counters;
